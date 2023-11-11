2203c2dfe96a7658f1818d57c6a139b0c29e2ab4
Avijit35 <avijit.manna@isubsoft.com>
2023-11-11 12:57:11 +0530

    Added Code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
-// import Textbox from "./components/Textbox";
-import About from "./components/About";
+import Textbox from "./components/Textbox";
+// import About from "./components/About";
+import React, {useState} from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if(mode === 'light')
+    {
+      setMode('dark')
+      document.body.style.backgroundColor = '#454549'
+      showAlert("Dark mode has been enabled", "success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
+    }
+  }
+
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+
+    setTimeout(()=>{
+      setAlert(null)
+    },3000)
+  }
+
   return (
     <>
-      < Navbar title = "TextUtils" aboutText = "about Us" />
+      < Navbar title = "TextUtils" aboutText = "about Us" mode = {mode} toggleFunc = {toggleMode}/>
+      <Alert alert = {alert}/>
       <div className="container my-3">
-          {/* <Textbox heading="Enter text here to analyse" /> */}
-          <About />
+          <Textbox heading="Enter text here to analyse" mode = {mode} showAlert = {showAlert}/>
+          {/* <About /> */}
       </div>
     </>
   );
--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -10,33 +10,68 @@ export default function Textbox(props) {
 
     const handleUpClick = () => {
         setText(text.toUpperCase())
+        props.showAlert("Convert to Upper Case", "success")
     }
 
     const handleLoClick = () => {
       setText(text.toLowerCase())
+      props.showAlert("Convert to Lower Case", "success")
   }
 
   const handleClearClick = () => {
     setText('')
-}
+    props.showAlert("Text are cleared", "success")
+  }
+
+  const handleFirstCapital = () => {
+    let arr = text.split(" ")
+    let newArr = arr.map(e => {
+      return e.charAt(0).toUpperCase() + e.slice(1)
+    })
+    
+    setText(newArr.join(" "))
+    props.showAlert("All first letter of words are capitalized", "success")
+  }
+
+  const handleCopy = () => {
+    // Get the text field
+    let copyText = document.getElementById("mybox");
+
+    // Select the text field
+    copyText.select();
+    // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    props.showAlert("Text copied to clipboard", "success")
+  }
+
+  const handleClearSpace = () => {
+    let arr = text.split(/[ ]+/)
+    setText(arr.join(" "))
+    props.showAlert("Extra Space has been cleared", "success")
+  }
     
   return (
     <>
-    <div className='container'>
-        <h1>{props.heading}</h1>
+    <div className='container' >
+        <h1 style={{ color: props.mode === 'light' ? 'black' : 'white'}}>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnchange} id="mybox" rows="8"></textarea>
+        <textarea className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : '#454549', color: props.mode === 'light' ? 'black' : 'white'}} value={text} onChange={handleOnchange} id="mybox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Upper Case</button>
         <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lower Case</button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick} >Clear Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleClearSpace} >Clear Extra Space</button>
+        <button className="btn btn-primary mx-1" onClick={handleFirstCapital} >Convert Capital Letter to all words</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white'}}>
       <h2>Your Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} minutes to read</p>
       <h2>Preview : </h2>
-      <p>{text}</p>
+      <p>{text.length > 0 ? text : "Please Enter something to analyse"}</p>
     </div>
     </>
   )
